37118e011a744a6834b1e80b07f24c1eb7e52530
rega-cfh <rega17@outlook.com>
2025-04-16 17:03:36 -0600

    Basic P2 Movement (Will Optimize later)

---
+++ Fire Escape Unity/Assets/Scripts/PlayerMovementScript.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovementScript : MonoBehaviour
+{
+    [SerializeField]
+    private float playerMoveSpeed;
+    [SerializeField]
+    private string map;
+
+    private Rigidbody2D rb;
+
+    private ControlMap inputActions;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        inputActions = new ControlMap();
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        OnPlayerMovement();
+    }
+
+    private void OnEnable()
+    {
+        inputActions.Enable();
+    }
+
+    private void OnDisable()
+    {
+        inputActions.Disable();
+    }
+
+    private void OnPlayerMovement()
+    {
+        //InputAction move = inputActions.FindAction("Movement");
+        Debug.Log("Inside");
+        Debug.Log(inputActions.PlayerGameplay1.Movement.ReadValue<Vector2>());
+        rb.linearVelocity = inputActions.PlayerGameplay1.Movement.ReadValue<Vector2>() * playerMoveSpeed;
+    }
+}
