340e133f35a1a8c8b94a0f9d779a9e46e8a89f78
Kenyou Teoh <137188714+Iforgotaboutitin2seconds@users.noreply.github.com>
2025-04-25 12:18:56 -0600

    Fixed multiplayer interaction
    - makes player 1 use E and player 2 use right shift
    - Makes it so player has to press the interaction key again to drop the object.

--- Fire Escape Unity/Assets/Scripts/PlayerInteraction.cs
+++ Fire Escape Unity/Assets/Scripts/PlayerInteraction.cs
@@ -2,9 +2,17 @@ using UnityEngine;
 
 public class PlayerInteraction : MonoBehaviour
 {
+
+    [SerializeField]
+    private int playerIndex = 0;
+
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E))
+        if (Input.GetKeyDown(KeyCode.E) && playerIndex == 0)
+        {
+            PlayerEventSystem.current.ObjectPickedUp();
+        }
+        else if (Input.GetKeyDown(KeyCode.RightShift) && playerIndex == 1)
         {
             PlayerEventSystem.current.ObjectPickedUp();
         }
