ddca0e8a050c8f7d87ab45d5c157f167a9ae0a65
Klax23 <puremaze23@gmail.com>
2025-10-10 02:59:45 -0600

    Sebastian Level/Level Select, Control/Help Screen, Tutorial Level Tool Tips for Tools/MoveableDebris

---
+++ Fire Escape Unity/Assets/Scripts/UIScripts/ToolTipDetection.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+//author alex
+public class ToolTipDetection : MonoBehaviour
+{
+    private bool tooltipsEnabled;
+    //only enable tooltips in the tutorial level
+    private void Start()
+    {
+        tooltipsEnabled = SceneManager.GetActiveScene().name == "TutorialLevel";
+    }
+    //If player collides with object with tooltip it will pop up the tooltip
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!tooltipsEnabled) return;
+
+        ToolTipIdentifier tooltip = other.GetComponent<ToolTipIdentifier>();
+
+        if (tooltip != null )
+        {
+            ToolTipManager.Instance.ShowToolTip(tooltip.tooltipMessage);
+        }
+    }
+
+    //If player collider leaves the object it will get rid of the tooltip
+    private void OnTriggerExit2D(Collider2D other)
+    { 
+        if (!tooltipsEnabled) return;
+
+        ToolTipIdentifier tooltip = other.GetComponent<ToolTipIdentifier>();
+        if (tooltip != null)
+        {
+            ToolTipManager.Instance.HideToolTip();
+        }
+    }
+}
---
+++ Fire Escape Unity/Assets/Scripts/UIScripts/ToolTipIdentifier.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+//author Alex
+public class ToolTipIdentifier : MonoBehaviour
+{
+    [TextArea]
+    public string tooltipMessage; // write whatever message you want to pop up for the tooltip goes into items prefabs
+}
