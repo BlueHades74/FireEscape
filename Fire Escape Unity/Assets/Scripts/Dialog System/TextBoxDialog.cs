2cfb3ec6ab965d562e0c4a252208bc497db7be59
SleeplessJake <jacobbiles.personal@gmail.com>
2025-11-12 09:17:03 -0700

    Merge 4
    9:16 am

--- Fire Escape Unity/Assets/Scripts/Dialog System/TextBoxDialog.cs
+++ Fire Escape Unity/Assets/Scripts/Dialog System/TextBoxDialog.cs
@@ -23,24 +23,27 @@ public class TextBoxDialog : MonoBehaviour
         // Get all NPCS
         foreach (GameObject n in GameObject.FindGameObjectsWithTag("NPC")) npcList.Add(n.GetComponent<QuestNPC>());
 
-        // Get all NPC text boxes
-        foreach (QuestNPC n in npcList) textBoxList.Add(n.GetComponentInChildren<TextMeshPro>());
-
-        // Deactivate all NPC text boxes
-        foreach (TextMeshPro t in textBoxList) t.transform.parent.gameObject.SetActive(false);
-
-        // Get Players
-        foreach (GameObject p in GameObject.FindGameObjectsWithTag("Player"))
+        if (npcList.Count > 0)
         {
-            if (p.name == "Player 1") playerOne = p;
-            else if (p.name == "Player 2") playerTwo = p;
+            // Get all NPC text boxes
+            foreach (QuestNPC n in npcList) textBoxList.Add(n.GetComponentInChildren<TextMeshPro>());
+
+            // Deactivate all NPC text boxes
+            foreach (TextMeshPro t in textBoxList) t.transform.parent.gameObject.SetActive(false);
+
+            // Get Players
+            foreach (GameObject p in GameObject.FindGameObjectsWithTag("Player"))
+            {
+                if (p.name == "Player 1") playerOne = p;
+                else if (p.name == "Player 2") playerTwo = p;
+            }
+
+            // Get player Text Boxes Objects
+            foreach (Transform c in playerOne.transform) if (c.gameObject.name == "Player 1 Textbox") playerOneTextUI = c.gameObject;
+            foreach (Transform c in playerTwo.transform) if (c.gameObject.name == "Player 2 Textbox") playerTwoTextUI = c.gameObject;
+
         }
 
-        // Get player Text Boxes Objects
-        foreach (Transform c in playerOne.transform) if (c.gameObject.name == "Player 1 Textbox") playerOneTextUI = c.gameObject;
-        foreach (Transform c in playerTwo.transform) if (c.gameObject.name == "Player 2 Textbox") playerTwoTextUI = c.gameObject;
-
-        Debug.Log(playerOneTextUI.name);
 
         // Get player Textmesh
         playerOneText = playerOneTextUI.GetComponentInChildren<TextMeshPro>();
@@ -53,39 +56,43 @@ public class TextBoxDialog : MonoBehaviour
 
     void Update()
     {
-        // Loop through all NPCS in a scence
-        for (int i = 0; i < npcList.Count; i++)
+        if (npcList.Count > 0)
         {
-            // Call QuestNPC.isPlayerWithinRange() -- to determine which and if player is in range
-            (int, bool) withinRange = npcList[i].isPlayerWithinRange();
 
-            // Swtich based on which character & if is in range
-            switch (withinRange)
+            // Loop through all NPCS in a scence
+            for (int i = 0; i < npcList.Count; i++)
             {
-                // Player 1 in range
-                case (1, true):
+                // Call QuestNPC.isPlayerWithinRange() -- to determine which and if player is in range
+                (int, bool) withinRange = npcList[i].isPlayerWithinRange();
 
-                    // Has Intial Dialog Triggered?
-                    if (npcList[i].HasHadInitialDialogTrigger == false) StartCoroutine(doInitialDialog(playerOne, playerOneText, playerOneTextUI, npcList[i], textBoxList[i], waitTime));
+                // Swtich based on which character & if is in range
+                switch (withinRange)
+                {
+                    // Player 1 in range
+                    case (1, true):
 
-                    // Has pickup Dialog Triggered?
-                    else if (npcList[i].HasHadPickupDialogTrigger == false && npcList[i].GetComponent<ObjectManager>().IsHeld == true) StartCoroutine(doPickupDialog(playerOne, playerOneText, playerOneTextUI, npcList[i], textBoxList[i], waitTime));
-                    break;
+                        // Has Intial Dialog Triggered?
+                        if (npcList[i].HasHadInitialDialogTrigger == false) StartCoroutine(doInitialDialog(playerOne, playerOneText, playerOneTextUI, npcList[i], textBoxList[i], waitTime));
 
-                // Player 2 in range
-                case (2, true):
-                    if (npcList[i].HasHadInitialDialogTrigger == false) StartCoroutine(doInitialDialog(playerTwo, playerTwoText, playerTwoTextUI, npcList[i], textBoxList[i], waitTime));
-                    else if (npcList[i].HasHadPickupDialogTrigger == false && npcList[i].GetComponent<ObjectManager>().IsHeld == true) StartCoroutine(doPickupDialog(playerTwo, playerTwoText, playerTwoTextUI, npcList[i], textBoxList[i], waitTime));
-                    break;
+                        // Has pickup Dialog Triggered?
+                        else if (npcList[i].HasHadPickupDialogTrigger == false && npcList[i].GetComponent<ObjectManager>().IsHeld == true) StartCoroutine(doPickupDialog(playerOne, playerOneText, playerOneTextUI, npcList[i], textBoxList[i], waitTime));
+                        break;
 
-                // No player in range
-                case (0, false):
-                    break;
+                    // Player 2 in range
+                    case (2, true):
+                        if (npcList[i].HasHadInitialDialogTrigger == false) StartCoroutine(doInitialDialog(playerTwo, playerTwoText, playerTwoTextUI, npcList[i], textBoxList[i], waitTime));
+                        else if (npcList[i].HasHadPickupDialogTrigger == false && npcList[i].GetComponent<ObjectManager>().IsHeld == true) StartCoroutine(doPickupDialog(playerTwo, playerTwoText, playerTwoTextUI, npcList[i], textBoxList[i], waitTime));
+                        break;
 
-                // Default -- I would be impressed if this ever happened
-                default:
-                    Debug.LogWarning("How did we get here? Well its because of variable data: " + withinRange);
-                    break;
+                    // No player in range
+                    case (0, false):
+                        break;
+
+                    // Default -- I would be impressed if this ever happened
+                    default:
+                        Debug.LogWarning("How did we get here? Well its because of variable data: " + withinRange);
+                        break;
+                }
             }
         }
     }
