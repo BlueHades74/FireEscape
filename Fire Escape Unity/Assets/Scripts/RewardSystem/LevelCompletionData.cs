0b69312e9b8dcdbd8d7a8afe8dc0cfa353c1746b
Klax23 <puremaze23@gmail.com>
2025-11-19 01:18:44 -0700

    Not much set yet, but this is the beginning

---
+++ Fire Escape Unity/Assets/Scripts/RewardSystem/LevelCompletionData.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class LevelCompletionData : MonoBehaviour
+{
+    [System.Serializable]
+    public class LevelResultData
+    {
+        public string levelName;
+
+        public int humansSaved;
+        public int totalHumans;
+
+        public float fireExtinguishedPercent;
+
+        public int bonusCollected;
+        public int bonusTotal;
+    }
+
+    public static class LevelResultCache
+    {
+        public static LevelResultData Data;
+    }
+}
