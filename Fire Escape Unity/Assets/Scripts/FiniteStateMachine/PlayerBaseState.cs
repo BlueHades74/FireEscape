53924de4270f84f667e339498cc5c748537f119b
BlueHades74 <156944645+BlueHades74@users.noreply.github.com>
2026-01-21 10:37:10 -0700

    idk

--- Fire Escape Unity/Assets/Scripts/FiniteStateMachine/PlayerBaseState.cs
+++ Fire Escape Unity/Assets/Scripts/FiniteStateMachine/PlayerBaseState.cs
@@ -1,16 +1,6 @@
 using UnityEngine;
 
-public class PlayerBaseState : MonoBehaviour
+public abstract class PlayerBaseState
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
