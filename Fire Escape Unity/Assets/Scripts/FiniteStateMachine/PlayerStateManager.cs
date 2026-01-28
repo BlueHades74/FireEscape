ad10d239f9bc672d01982f9c648abff6f8907e2c
BlueHades74 <156944645+BlueHades74@users.noreply.github.com>
2026-01-27 22:08:17 -0700

    State Machine
    Added finite state machine and fixed player idle facing directions

--- Fire Escape Unity/Assets/Scripts/FiniteStateMachine/PlayerStateManager.cs
+++ Fire Escape Unity/Assets/Scripts/FiniteStateMachine/PlayerStateManager.cs
@@ -2,15 +2,31 @@ using UnityEngine;
 
 public class PlayerStateManager : MonoBehaviour
 {
+    PlayerBaseState currentState;
+    public PlayerFaceDown faceDown = new PlayerFaceDown();
+    public PlayerFaceLeft faceLeft = new PlayerFaceLeft();
+    public PlayerFaceRight faceRight = new PlayerFaceRight();
+    public PlayerFaceUp faceUp = new PlayerFaceUp();
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        // starting state
+        currentState = faceDown;
+
+        currentState.EnterState(this);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        currentState.UpdateState(this);
+    }
+
+    public void SwitchState(PlayerBaseState state)
+    {
+        currentState = state;
+
+        state.EnterState(this);
     }
 }
