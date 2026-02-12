9e6aedbd57f967cd3f512f13e1d399fa2f7dbd0f
BlueHades74 <156944645+BlueHades74@users.noreply.github.com>
2026-02-11 22:26:26 -0700

    fixed up for playtest

--- Fire Escape Unity/Assets/Scripts/Objects/BreakableObjectScript.cs
+++ Fire Escape Unity/Assets/Scripts/Objects/BreakableObjectScript.cs
@@ -1,25 +1,35 @@
+using NUnit.Framework;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class BreakableObjectScript : MonoBehaviour
 {
-    [SerializeField]
+    // making healthPoints a SerializeField will break the animation
     private int healthPoints;
-    private TextMeshProUGUI text; 
+    private TextMeshProUGUI text;
+
+    // Door destroyed animation variables
+    private Animator animator;
+    // bool for whether door just explode or not, true = yes
+    [SerializeField] private bool popDoor;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         healthPoints = 3;
-        text = transform.GetChild(0).transform.GetChild(0).GetComponent<TextMeshProUGUI>();
-        
+        //text = transform.GetChild(0).transform.GetChild(0).GetComponent<TextMeshProUGUI>();
+
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (popDoor)
+        {
+            gameObject.SetActive(false);
+        }
     }
 
     /// <summary>
@@ -28,13 +38,15 @@ public class BreakableObjectScript : MonoBehaviour
     public void DamageBreakable()
     {
         healthPoints--;
-        text.text = healthPoints.ToString();
+        //text.text = healthPoints.ToString();
 
-        if (healthPoints <= 0)
-        {
-            gameObject.GetComponent<BoxCollider2D>().enabled = false;
-            gameObject.GetComponent<SpriteRenderer>().enabled = false;
-            text.text = "";
-        }
+        animator.SetInteger("TimesHit", healthPoints);
+
+        //if (healthPoints <= 0)
+        //{
+            //gameObject.GetComponent<BoxCollider2D>().enabled = false;
+            //gameObject.GetComponent<SpriteRenderer>().enabled = false;
+            //text.text = "";
+        //}
     }    
 }
