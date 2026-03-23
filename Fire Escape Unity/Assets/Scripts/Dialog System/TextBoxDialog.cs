3c5644791c716581113edadd8db5576778eed374
rega-cfh <rega17@outlook.com>
2026-03-23 11:02:51 -0600

    DialogFix

--- Fire Escape Unity/Assets/Scripts/Dialog System/TextBoxDialog.cs
+++ Fire Escape Unity/Assets/Scripts/Dialog System/TextBoxDialog.cs
@@ -100,6 +100,7 @@ public class TextBoxDialog : MonoBehaviour
             catch
             {
                 npcList.RemoveAt(i);
+                textBoxList.RemoveAt(i);
             }
         }
     }
