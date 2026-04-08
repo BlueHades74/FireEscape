39ed0a1efdd8512f6a92c0c04da30ec9dc1212b9
Glimmerfrost <srosul@uccs.edu>
2026-04-08 10:47:00 -0600

    OnHoverUIPart02
    This is part 2 of the last push, the hovering actually works now.

--- Fire Escape Unity/Assets/TextMesh Pro/UI Scripts/OnHoverUI.cs
+++ Fire Escape Unity/Assets/TextMesh Pro/UI Scripts/OnHoverUI.cs
@@ -1,11 +1,17 @@
+//Created by Sebastian Rosul on 04_08_2026 
+//The purpose is so when the mouse hovers over the tool icons, it gives a brief description of what each tool is 
 using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class OnHoverUI : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
-    [SerializeField] 
-    public GameObject ToolDescriptor; 
-    
+    [SerializeField]
+    public GameObject ToolDescriptor;
+    private void Start()
+    {
+        ToolDescriptor.SetActive(false); 
+    }
+
     public void OnPointerEnter(PointerEventData eventData)
     {
         ToolDescriptor.SetActive(true);
